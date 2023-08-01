8017ee9d1f1cb4f0ce0011dcd1a4605a1d8a5bb2
Wm. Brett Henry <wbh1942@gmail.com>
2023-08-01 17:35:45 -0500

    Create Derived Character Classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright BirdyB LLC
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright BirdyB LLC
+
+
+#include "Character/AuraEnemy.h"
+
